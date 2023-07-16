89e997eec46148f916f3adec355de9db49d45f1e
bizhangjie <47079186+bizhangjie@users.noreply.github.com>
2023-07-16 10:24:39 +0800

    Update tv.ihoho.js

--- repo/tv.ihoho.js
+++ repo/tv.ihoho.js
@@ -122,8 +122,15 @@ export default class extends Extension {
     }
 
     async detail(url) {
-        const res = await this.request(url)
-
+        const res = await this.request(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+                Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                Connection: 'keep-alive',
+            }
+        });
         // const res = await axios.get('https://ihoho.tv/detail/201955.html', {
         //     headers: {
         //         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
